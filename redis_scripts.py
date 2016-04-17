977112d9953841001e253d4ee95be98d6beacaf1
Leif K-Brooks <eurleif@gmail.com>
2016-04-17 09:45:44 +0000

    Allow custom Lua table name (defaulting to the name of the Python class).

--- redis_scripts.py
+++ redis_scripts.py
@@ -6,22 +6,36 @@ except:
     import json
 
 class Scripts(object):
-    def __new__(cls, *args):
-        if len(args) == 1 and isclass(args[0]):
-            return Scripts(None, args[0])
+    def __new__(cls, *args, **kwargs):
+        if len(args) == 0 and kwargs:
+            return partial(Scripts, **kwargs)
+        elif len(args) == 1 and isclass(args[0]):
+            return Scripts(None, args[0], **kwargs)
         elif len(args) == 1: # redis
-            return partial(Scripts, args[0])
+            return partial(Scripts, args[0], **kwargs)
         else:
-            redis, scripts = args
+            if len(args) == 2:
+                redis, scripts = args
+                table_name = None
+            elif len(args) == 3:
+                redis, scripts, table_name = args
+            else:
+                raise TypeError('Wrong number of args')
+            if table_name is None:
+                table_name = kwargs.pop('table_name', None)
+            if kwargs:
+                raise TypeError('Excess kwargs: %s' % kwargs)
+            if table_name is None:
+                table_name = getattr(scripts, '__name__', 'S')
+            
             self = object.__new__(cls)
             if isclass(scripts):
                 scripts = self._extract_lua_from_class(scripts)
             self._scripts_ = dict(scripts)
-            compiled_script = self._compile_script(self._scripts_)
+            self._compiled_script = self._compile_script(table_name, self._scripts_)
             if redis is not None:
-                self._registered_script = redis.register_script(compiled_script)
+                self._registered_script = redis.register_script(self._compiled_script)
             else:
-                self._compiled_script = compiled_script
                 self._registered_script = None
             self._redis_ = redis
             return self
@@ -38,7 +52,7 @@ class Scripts(object):
                 continue
             yield method.__name__, (argspec.args, method.__doc__)
 
-    def _compile_script(self, scripts):
+    def _compile_script(self, table_name, scripts):
         table_entries = []
         for name, (args, code) in scripts.iteritems():
             function = 'function(%s) %s end' % (', '.join(args), code)
@@ -46,14 +60,14 @@ class Scripts(object):
             table_entries.append(table_entry)
         table = '{' + ', '.join(table_entries) + '}'
         return '''
-            local S
-            S = %s
+            local {table_name}
+            {table_name} = {table}
             do
                 local name = ARGV[1]
                 local argv = cjson.decode(ARGV[2])
-                return cjson.encode(S[name](unpack(argv)) or nil)
+                return cjson.encode({table_name}[name](unpack(argv)) or nil)
             end
-        ''' % table
+        '''.format(table=table, table_name=table_name)
 
     def _call_(self, name, *args, **kwargs):
         redis = kwargs.pop('redis', None)
